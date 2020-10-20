4441fc10ea346a061cbdf32cfc166b7163bd75b8
Deepali Garg <73010651+deeps0102@users.noreply.github.com>
2020-10-20 14:33:42 +0530

    updated customer class

--- customer.java
+++ customer.java
@@ -1,4 +1,17 @@
-class customer
+public class customer
 {
-	;
+	private int customer_id;
+	private string c_name;
+	private string c_address;
+	private int c_phone_no; 
+
+	public customer(int customer_id, string c_name, string c_address, int c_phone_no)
+	{
+		//define the constructor
+	}
+
+	public string getcustomer_id(){
+		return this.customer_id;
+	}
+	
 }
