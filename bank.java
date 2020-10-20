638f45a7c2584b61546dae70723c49e2d0caf2cd
Deepali Garg <73010651+deeps0102@users.noreply.github.com>
2020-10-20 14:35:40 +0530

    updated bank class

--- bank.java
+++ bank.java
@@ -1,4 +1,31 @@
-class bank
+public class bank
 {
-	;
+	private int bank_id;
+	private string bank_name;
+	private int IFSC_code;
+	private int branch_id;
+	private string bank_address;   
+	private ArrayList<customer> customers;
+	private ArrayList<account> accounts;
+
+	public bank()
+	{
+		//definr constructor
+	}
+
+	public string getName(){
+		//gets bank name
+	}
+
+	public int getBankID(){
+		//returns bank id
+	}
+
+	public int getBranchID(){
+		//returns branch id
+	}
+
+	public customer login(){
+		//checks if customer is registered
+	}
 }
