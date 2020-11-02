072bb2e1b94b25176847bc23e5f374dc594e2025
Deepali Garg <gargdivya1002@gmail.com>
2020-11-02 21:55:12 +0530

    Updated Card.java

--- Card.java
+++ Card.java
@@ -2,53 +2,55 @@ public class Card
 {
 	private int card_no;
 	private int exp_date;
-	private string card_type;
-	private int Pin;
+	private String card_type;
+	private int pin;
 
-	public card(int card_no, int exp_date, int Pin){
-		this.setcard_no(card_no);
-		this.setexp_date(exp_date);
-		this.setcard_type(card_no);
-		this.setPin(Pin);
+	public card(int card_no, int exp_date, int pin)
+	{
+		//defining the cunstructor
+		this.setCard_no(card_no);
+		this.setExp_date(exp_date);
+		this.setCard_type(card_no);
+		this.setPin(pin);
 	}
 
 	//returns card number
-	public int getcard_no(){
+	public int getCard_no(){
 		return this.card_no;
 	}
 
 	//set card number
-	public void setcard_no(int card_no){
+	public void setCard_no(int card_no){
 		this.card_no = card_no;
 	}
 
 	//returns card number
-	public int getexp_date(){
+	public int getExp_date(){
 		return this.exp_date;
 	}
 
 	//set card number
-	public void setexp_date(int exp_date){
+	public void setExp_date(int exp_date){
 		this.exp_date = exp_date;
 	}
 
 	//returns card number
 	public int getPin(){
-		return this.Pin;
+		return this.pin;
 	}
 
 	//set card number
-	public void setPin(int Pin){
-		this.Pin = Pin;
+	public void setPin(int pin){
+		this.pin = pin;
 	}
 
 	//returns card number
-	public string getcard_type(){
+	public String getCard_type(){
 		return this.card_type;
 	}
 
 	//set card number
-	public void setcard_type(int card_no){
+	public void setCard_type(int card_no){
 		int temp = card_no;
 		while(temp>=10)
 			temp /= 10;
@@ -71,7 +73,7 @@ public class Card
 		returns true if pin is correct
 		returns false if pin is incorrect
 	*/
-	public boolean ValidPin(int pin){
+	public boolean ValidPin(int cardNo, int pin){
 
 	}
 
@@ -81,4 +83,12 @@ public class Card
 	public Card find_user(int cardNo, int pin){
 
 	}
+
+	/*
+		upadates the PIN of user
+	*/
+	public void updatePin(int cardNo, int newPin){
+		
+	}
 }
+
